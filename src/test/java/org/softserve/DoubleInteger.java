3a939d0b372689cdc08450a7c9a2d2004f2132de
Anastasia Zdvizhkova <nastyailkiv@gmail.com>
2021-08-29 19:11:37 +0300

    init commit

---
+++ src/test/java/org/softserve/DoubleInteger.java
@@ -0,0 +1,10 @@
+package org.softserve;
+
+public class DoubleInteger {
+
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i * 2;
+    }
+
+}
