384cba29e2b2e74b07e15c7c3fe36a2388e2c7cf
Anastasia Zdvizhkova <nastyailkiv@gmail.com>
2021-09-04 00:33:40 +0300

    cuboid volume

---
+++ src/test/java/org/softserve/CuboidVolume.java
@@ -0,0 +1,9 @@
+package org.softserve;
+
+public class CuboidVolume {
+    
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        // Your code here
+        return length * width * height;
+    }
+}
