74f55bc1cd5f9511734bb9554886db5156274603
Anastasia Zdvizhkova <nastyailkiv@gmail.com>
2021-09-04 00:31:55 +0300

    multiplying two numbers

---
+++ src/test/java/org/softserve/MultiplyingTwoNumbers.java
@@ -0,0 +1,10 @@
+package org.softserve;
+
+public class MultiplyingTwoNumbers {
+
+    public static int multiply(int num1, int num2) {
+        // your code goes here
+        return num1 * num2;
+    }
+}
+
