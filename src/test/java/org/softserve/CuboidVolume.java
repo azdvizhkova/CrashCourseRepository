eb4f88f91ec1a6ea2955631e54e5eb33b5e51ceb
Anastasia Zdvizhkova <nastyailkiv@gmail.com>
2021-09-04 00:36:09 +0300

    number drills

--- src/test/java/org/softserve/CuboidVolume.java
+++ src/test/java/org/softserve/CuboidVolume.java
@@ -1,7 +1,7 @@
 package org.softserve;
 
 public class CuboidVolume {
-    
+
     public static double getVolumeOfCuboid(final double length, final double width, final double height) {
         // Your code here
         return length * width * height;
---
+++ src/test/java/org/softserve/NumberDrills.java
@@ -0,0 +1,8 @@
+package org.softserve;
+
+public class NumberDrills {
+
+    public static boolean isDivisible(int wallLength, int pixelSize) {
+        return wallLength % pixelSize == 0;
+    }
+}
