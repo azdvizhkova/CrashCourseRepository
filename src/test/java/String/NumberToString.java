80912250e4d5679c04e8508efe5ee0a60d485261
Anastasia Zdvizhkova <nastyailkiv@gmail.com>
2021-09-18 12:37:26 +0300

    number to string

---
+++ src/test/java/String/NumberToString.java
@@ -0,0 +1,10 @@
+package String;
+
+public class NumberToString {
+
+    public static String numberToString(int num) {
+        // Return a string of the number here!
+        String str = Integer.toString(num);
+        return str;
+    }
+}
